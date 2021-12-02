62c349873ca299647b509778ea5037cfb5f55aeb
Cohen Carlisle <cohencarlisle+github@pm.me>
2021-12-01 20:05:51 -0500

    day 1 part 1

--- day-1/sonar_sweep/lib/sonar_sweep.ex
+++ day-1/sonar_sweep/lib/sonar_sweep.ex
@@ -1,18 +1,37 @@
 defmodule SonarSweep do
   @moduledoc """
-  Documentation for `SonarSweep`.
+  A module for performing real live sonar sweeps of the sea floor.
   """
 
   @doc """
-  Hello world.
+  Calculates the number of increases from the previous measurement for all
+  elements in a list.
 
   ## Examples
 
-      iex> SonarSweep.hello()
-      :world
+      iex> SonarSweep.count_increases([1, 2, 3])
+      2
+      iex> SonarSweep.count_increases([3, 2, 1])
+      0
+      iex> SonarSweep.count_increases([1, 2, 2, 1, 3, 5])
+      3
 
   """
-  def hello do
-    :world
+  def count_increases(depths) do
+    depths
+    |> Enum.reduce([count: 0], &do_count_increases/2)
+    |> Keyword.fetch!(:count)
+  end
+
+  defp do_count_increases(current, [count: 0]) do
+    [previous: current, count: 0]
+  end
+
+  defp do_count_increases(current, [previous: previous, count: count]) when current > previous do
+    [previous: current, count: count + 1]
+  end
+
+  defp do_count_increases(current, [previous: _previous, count: count]) do
+    [previous: current, count: count]
   end
 end
--- day-1/sonar_sweep/test/sonar_sweep_test.exs
+++ day-1/sonar_sweep/test/sonar_sweep_test.exs
@@ -2,7 +2,15 @@ defmodule SonarSweepTest do
   use ExUnit.Case
   doctest SonarSweep
 
-  test "greets the world" do
-    assert SonarSweep.hello() == :world
+  describe "#count_increases/1" do
+    test "counts the increases correctly" do
+      measurements =
+        "test/fixtures/depth_measurements.txt"
+        |> File.read!()
+        |> String.split("\n", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      assert SonarSweep.count_increases(measurements) == 1832
+    end
   end
 end
