951d2639c08786b4404639eac2c3ff217fc01f77
Cohen Carlisle <cohencarlisle+github@pm.me>
2021-12-01 20:41:32 -0500

    day 1 part 2

--- day-1/sonar_sweep/lib/sonar_sweep.ex
+++ day-1/sonar_sweep/lib/sonar_sweep.ex
@@ -4,8 +4,8 @@ defmodule SonarSweep do
   """
 
   @doc """
-  Calculates the number of increases from the previous measurement for all
-  elements in a list.
+  Calculates the number of increases from the previous measurement over all
+  elements in a list. If given a window, compares the sum of the sliding window.
 
   ## Examples
 
@@ -13,25 +13,31 @@ defmodule SonarSweep do
       2
       iex> SonarSweep.count_increases([3, 2, 1])
       0
-      iex> SonarSweep.count_increases([1, 2, 2, 1, 3, 5])
+      iex> SonarSweep.count_increases([1, 2, 2, 3, 3])
+      2
+      iex> SonarSweep.count_increases([1, 2, 2, 3, 3], 2)
       3
 
   """
-  def count_increases(depths) do
+  def count_increases(depths, window \\ 1) do
     depths
-    |> Enum.reduce([count: 0], &do_count_increases/2)
+    |> Enum.reduce([count: 0, previous: [], window: window], &do_count_increases/2)
     |> Keyword.fetch!(:count)
   end
 
-  defp do_count_increases(current, [count: 0]) do
-    [previous: current, count: 0]
+  defp do_count_increases(current, [count: 0, previous: previous, window: window]) when length(previous) < window do
+    [count: 0, previous: [current | previous], window: window]
   end
 
-  defp do_count_increases(current, [previous: previous, count: count]) when current > previous do
-    [previous: current, count: count + 1]
-  end
+  defp do_count_increases(current, [count: count, previous: previous, window: window]) do
+    new_previous = [current | Enum.slice(previous, 0..-2)]
+    previous_sum = Enum.sum(previous)
+    current_sum = Enum.sum(new_previous)
 
-  defp do_count_increases(current, [previous: _previous, count: count]) do
-    [previous: current, count: count]
+    if current_sum > previous_sum do
+      [count: count + 1, previous: new_previous, window: window]
+    else
+      [count: count, previous: new_previous, window: window]
+    end
   end
 end
--- day-1/sonar_sweep/test/sonar_sweep_test.exs
+++ day-1/sonar_sweep/test/sonar_sweep_test.exs
@@ -2,8 +2,8 @@ defmodule SonarSweepTest do
   use ExUnit.Case
   doctest SonarSweep
 
-  describe "#count_increases/1" do
-    test "counts the increases correctly" do
+  describe "#count_increases/2" do
+    test "counts the increases correctly with no window" do
       measurements =
         "test/fixtures/depth_measurements.txt"
         |> File.read!()
@@ -11,6 +11,16 @@ defmodule SonarSweepTest do
         |> Enum.map(&String.to_integer/1)
 
       assert SonarSweep.count_increases(measurements) == 1832
+    end
+
+    test "counts the increases correctly with a window of 3" do
+      measurements =
+        "test/fixtures/depth_measurements.txt"
+        |> File.read!()
+        |> String.split("\n", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      assert SonarSweep.count_increases(measurements, 3) == 1858
     end
   end
 end
